b09e5fb7fae92ebd88dc75aa37bb94e61cce0441
SvitlanaHonchar <snidlty@gmail.com>
2022-12-13 21:09:45 +0200

    Task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+// створюємо масив елементів списку
+const ingredientElements = ingredients.map((ingredient) => {
+  const ingredientItem = document.createElement("li");
+  ingredientItem.classList.add("item");
+  ingredientItem.textContent = ingredient;
+
+  return ingredientItem;
+});
+
+// шукаємо ul і додаємо елементи в список
+const ingredientList = document.querySelector("#ingredients");
+ingredientList.append(...ingredientElements);
