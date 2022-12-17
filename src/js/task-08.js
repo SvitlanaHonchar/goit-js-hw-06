45f9f3dfc176ff7b59591f20af266e84418f529b
SvitlanaHonchar <snidlty@gmail.com>
2022-12-17 10:52:06 +0200

    Task-08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,37 @@
+// знаходимо форму
+// const emailInputEl = document.querySelector("input[type = 'email']");
+// const passwordInputEl = document.querySelector("input[type = 'password']");
+// const loginBtn = document.querySelector("button");
+const formEl = document.querySelector(".login-form");
 
+// об'єкт для зберігання даних
+
+const userLoginInfo = {
+  email: "",
+  password: "",
+};
+
+// подія
+formEl.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  // console.log("submit event");
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Alert! All fields should be filled in!");
+    return;
+  }
+
+  userLoginInfo.email = email.value;
+  userLoginInfo.password = password.value;
+  console.log(userLoginInfo);
+
+  // очищення форми
+  email.value = "";
+  password.value = "";
+}
