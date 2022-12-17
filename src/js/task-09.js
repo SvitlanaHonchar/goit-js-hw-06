4a1295c8e74099e95495ad46a38976b81ccc5e31
SvitlanaHonchar <snidlty@gmail.com>
2022-12-17 12:03:06 +0200

    Task-09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+// знаходимо елементи
+const buttonEl = document.querySelector("button");
+const bodyEl = document.querySelector("body");
+const spanEl = document.querySelector("span");
+
+// функція
+function onChangeColorBtnElClick() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
+}
+
+// відстеження кліку
+buttonEl.addEventListener("click", onChangeColorBtnElClick);
